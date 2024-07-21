97c0b2ba30d86a0afb55d3a5f5ecac42b5448cc7
PaBah <paul.bahushevich@gmail.com>
2024-07-21 17:10:04 +0300

    refactored

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -25,7 +25,6 @@ func main() {
 	fmt.Printf("Build version: %s\n", buildVersion)
 	fmt.Printf("Build date: %s\n", buildDate)
 	fmt.Printf("Build commit: %s\n", buildCommit)
-
 	options := &config.Options{}
 	ParseFlags(options)
 
