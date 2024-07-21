b0e38f42d872ea60150af6710848ba2bf20330a0
PaBah <paul.bahushevich@gmail.com>
2024-07-21 22:14:56 +0300

    added base config

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"os"
 	"strconv"
 
@@ -24,7 +25,8 @@ func ParseFlags(options *config.Options) {
 	var specified bool
 	var serverAddress, baseURL, logsLevel, fileStoragePath, databaseDSN, enableHTTPS, configFilePath string
 
-	flag.StringVar(&configFilePath, "c", "/Users/paulbahush/projects/yp/url-shortener/config.json", "path to config file")
+	//flag.StringVar(&configFilePath, "c", "/Users/paulbahush/projects/yp/url-shortener/config.json", "path to config file")
+	flag.StringVar(&configFilePath, "c", "", "path to config file")
 	flag.StringVar(&options.ServerAddress, "a", "", "host:port on which server run")
 	flag.StringVar(&options.BaseURL, "b", "", "URL for of shortened URLs hosting")
 	flag.StringVar(&options.DatabaseDSN, "d", "", "database DSN address")
@@ -33,27 +35,35 @@ func ParseFlags(options *config.Options) {
 	flag.BoolVar(&options.EnableHTTPS, "s", false, "enable-https")
 	flag.Parse()
 
-	if !isFlagPassed("c") {
-		var fileConfig config.Options
+	if configFilePath != "" {
+		defaultConfig := config.Options{
+			ServerAddress:   ":8080",
+			BaseURL:         "http://localhost:8080",
+			LogsLevel:       "info",
+			FileStoragePath: "/tmp/short-url-db.json",
+			EnableHTTPS:     false,
+			DatabaseDSN:     "host=localhost user=paulbahush dbname=urlshortener password=",
+		}
 		file, err := os.Open(configFilePath)
 		defer file.Close()
 		if err == nil {
-			err = json.NewDecoder(file).Decode(&fileConfig)
+			err = json.NewDecoder(file).Decode(&defaultConfig)
+			fmt.Println(defaultConfig)
 			if err == nil {
 				if !isFlagPassed("a") {
-					options.ServerAddress = fileConfig.ServerAddress
+					options.ServerAddress = defaultConfig.ServerAddress
 				}
 				if !isFlagPassed("b") {
-					options.BaseURL = fileConfig.BaseURL
+					options.BaseURL = defaultConfig.BaseURL
 				}
 				if !isFlagPassed("d") {
-					options.DatabaseDSN = fileConfig.DatabaseDSN
+					options.DatabaseDSN = defaultConfig.DatabaseDSN
 				}
 				if !isFlagPassed("f") {
-					options.FileStoragePath = fileConfig.FileStoragePath
+					options.FileStoragePath = defaultConfig.FileStoragePath
 				}
 				if !isFlagPassed("s") {
-					options.EnableHTTPS = fileConfig.EnableHTTPS
+					options.EnableHTTPS = defaultConfig.EnableHTTPS
 				}
 			}
 		}
