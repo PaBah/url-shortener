2fb3b806621b6c4de1aef63b8eee92055cf94d0f
PaBah <paul.bahushevich@gmail.com>
2024-08-05 21:45:51 +0300

    updated storage mock

--- internal/middlewares/ip_whitelist.go
+++ internal/middlewares/ip_whitelist.go
@@ -6,19 +6,23 @@ import (
 	"net/http"
 )
 
+// IPWhiteListMiddleware return middleware handler
 func IPWhiteListMiddleware(n *net.IPNet) func(next http.Handler) http.Handler {
 	whiteLister := NewIPWhiteLister(n)
 	return whiteLister.Handler
 }
 
+// IPWhiteList struct with white list middleware data
 type IPWhiteList struct {
 	subnet *net.IPNet
 }
 
+// NewIPWhiteLister creates new IPWhiteList
 func NewIPWhiteLister(subnet *net.IPNet) *IPWhiteList {
 	return &IPWhiteList{subnet: subnet}
 }
 
+// Handler IPWhiteList middlewares handler
 func (wl *IPWhiteList) Handler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ip := net.ParseIP(r.Header.Get("X-Real-IP"))
